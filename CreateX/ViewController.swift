09370c6c6e9d9366a3095e3ba5ae71c77bea8698
Truong, Christopher Taylor <christruong28@gmail.com>
2018-11-19 17:47:51 -0500

    Initial Commit

---
+++ CreateX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CreateX
+//
+//  Created by Chris Truong on 11/19/18.
+//  Copyright © 2018 Chris Truong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
