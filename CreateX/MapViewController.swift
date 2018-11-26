d2654115a83d5c46824ef611935dcf13e5ae9709
Truong, Christopher Taylor <christruong28@gmail.com>
2018-11-26 15:18:56 -0500

    added pins and location

--- CreateX/MapViewController.swift
+++ CreateX/MapViewController.swift
@@ -7,13 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: BaseViewController {
+class MapViewController: BaseViewController, CLLocationManagerDelegate, MKMapViewDelegate   {
     
     var data = [Violation]()
     var one: Violation = Violation()
     var two: Violation = Violation()
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    var annotation = MKPointAnnotation()
+    var annotation2 = MKPointAnnotation()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,8 +43,60 @@ class MapViewController: BaseViewController {
         
         data.append(one)
         data.append(two)
+        
+        let location = one.location
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location!) { [weak self] placemarks, error in
+            if let placemark = placemarks?.first, let location = placemark.location {
+                let mark = MKPlacemark(placemark: placemark)
+                
+                if var region = self?.mapView.region {
+                    region.center = location.coordinate
+                    region.span.longitudeDelta /= 8.0
+                    region.span.latitudeDelta /= 8.0
+                    self?.mapView.setRegion(region, animated: true)
+                    self?.mapView.addAnnotation(mark)
+                }
+            }
+        }
+        
+        let location2 = two.location
+        let geocoder2 = CLGeocoder()
+        geocoder2.geocodeAddressString(location2!) { [weak self] placemarks, error in
+            if let placemark = placemarks?.first, let location2 = placemark.location {
+                let mark = MKPlacemark(placemark: placemark)
+                
+                if var region = self?.mapView.region {
+                    region.center = location2.coordinate
+                    region.span.longitudeDelta /= 8.0
+                    region.span.latitudeDelta /= 8.0
+                    self?.mapView.setRegion(region, animated: true)
+                    self?.mapView.addAnnotation(mark)
+                }
+            }
+        }
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        DispatchQueue.main.async {
+            self.locationManager.startUpdatingLocation()
+        }
+        
+        mapView.addAnnotation(annotation)
+        mapView.addAnnotation(annotation2)
+        
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last as! CLLocation
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        var region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        region.center = mapView.userLocation.coordinate
+        //mapView.setRegion(region, animated: true)
+    }
 
     /*
     // MARK: - Navigation
